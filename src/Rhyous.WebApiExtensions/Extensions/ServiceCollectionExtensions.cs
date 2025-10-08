b34a22cf895ab59e92d92470b025ec09cd397f5d
Jared Barneck <jaredbarneck@auctiontechnologygroup.com>
2025-10-08 10:02:35 -0600

    Add RegisterConfiguration

--- src/Rhyous.WebApiExtensions/Extensions/ServiceCollectionExtensions.cs
+++ src/Rhyous.WebApiExtensions/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Rhyous.WebApiExtensions.Interfaces;
 
 namespace Rhyous.WebApiExtensions;
@@ -15,5 +17,24 @@ public static class ServiceCollectionExtensions
     {
         var module = Activator.CreateInstance(typeof(T), constructorParams) as IDependencyInjectionModule;
         module!.Register(services);
+    }
+
+    /// <summary>Registers a configuration section as a singleton service.</summary>
+    /// <typeparam name="TInterface">The interface type.</typeparam>
+    /// <typeparam name="TConcrete"></typeparam>
+    /// <param name="services">An instance of <see cref="IServiceCollection"/>.</param>
+    /// <param name="config">An instance of <see cref="IConfiguration"/> from which to get the section.</param>
+    /// <param name="configSectionName">Optional. The name of the configuration section to bind to the concrete type. The default is the class name of the concrete type.</param>
+    /// <param name="registerConcrete">Optional. If true, registers the concrete type as well as the interface. Default is false. It is not recommended to register the concrete type.</param>
+    public static void RegisterConfiguration<TInterface, TConcrete>(this IServiceCollection services, IConfiguration config, string? configSectionName = null, bool registerConcrete = false)
+        where TConcrete : class, TInterface
+        where TInterface : class
+    {
+        configSectionName ??= typeof(TConcrete).Name;
+        services.Configure<TConcrete>(config.GetSection(configSectionName));
+        services.AddSingleton<TInterface>(c => c.GetService<IOptions<TConcrete>>()!.Value);
+        if (registerConcrete)
+            services.AddSingleton<TConcrete>(c => c.GetService<IOptions<TConcrete>>()!.Value);
+
     }
 }
